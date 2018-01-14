6e0ebbb1c8e681d7b0696b17443aa73a84280753
spyridon97 <spyrion97@hotmail.com>
2018-01-14 13:22:56 +0200

    -fixing comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,25 +5,5 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         App.Launch(args);
-
-        /*
-        TdmaSimulator tdmaSimulator = new TdmaSimulator(8, 6, 100000);
-        ArrayList<Integer> meanBurstLengths = new ArrayList<>();
-
-        meanBurstLengths.add(6);
-        meanBurstLengths.add(6);
-        meanBurstLengths.add(5);
-        meanBurstLengths.add(5);
-        meanBurstLengths.add(5);
-        meanBurstLengths.add(4);
-        meanBurstLengths.add(4);
-        meanBurstLengths.add(4);
-
-        tdmaSimulator.runAtdmaSimulator(0.18, meanBurstLengths);
-
-        System.out.println("Throughput =  " + tdmaSimulator.getThroughput());
-        System.out.println("Average Delay Time =  " + tdmaSimulator.getAverageDelayTimeSlots());
-        //*/
-
     }
 }
